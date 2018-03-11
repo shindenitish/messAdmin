584c7c976bae2b8171b767276cec55eb14ced170
shindenitish <shindenitish@yahoo.com>
2018-03-11 16:14:57 +0530

    PWA Updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 
@@ -45,8 +43,6 @@ const config = {
     LoginPage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
     AuthProvider
   ]
